c5580de182d3e9aff7849f95dad245016ced6f82
fadul <fadul@pop-os.localdomain>
2023-06-09 14:06:28 -0300

    Initial commit

---
+++ src/main/kotlin/P01-KotlinBasico.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Olá Mundo Kotlin!!")
+
+    // declaração de variáveis
+    var nome: String = "Gabriel"
+    // poderia ficar com o tipo inferido
+    var ender = "Rua nova, 25"
+    nome = "Gabriel da Silva"
+    println(nome)
+}
